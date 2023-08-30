d803c055a9d043bcbc3fc5285514db78f1dc9d40
Rudy Berends <rudy.berends@visolity.nl>
2023-08-30 11:00:36 +0000

    code cleanup

--- src/loxone/items/IntercomV2.ts
+++ src/loxone/items/IntercomV2.ts
@@ -11,9 +11,7 @@ export class IntercomV2 extends Intercom {
       this.platform.log.debug(`[${this.device.name}] Found Loxone Intercom on IP: ${ip}`);
 
       this.setupCamera(`http://${ip}/mjpg/video.mjpg`);
-
-      // Fetch Intercon MotionSensor;
-      this.configureMotionSensor();
+      this.configureMotionSensor();  // Fetch Intercon MotionSensor;
     });
   }
 
@@ -22,17 +20,15 @@ export class IntercomV2 extends Intercom {
     const intercomMotionUuid = Object.keys(this.platform.LoxoneItems).filter(uuid => uuid.startsWith(targetUuidPrefix));
 
     if (intercomMotionUuid) {
-      const matchingDevice = this.platform.LoxoneItems[`${intercomMotionUuid}`];
       this.platform.log.debug(`[${this.device.name}] Found Loxone Intercom MotionSensor`);
 
+      const matchingDevice = this.platform.LoxoneItems[`${intercomMotionUuid}`];
       const serviceName = matchingDevice.name.replace(/\s/g, ''); // Removes all spaces
       for (const stateName in matchingDevice.states) {
         const stateUUID = matchingDevice.states[stateName];
         this.ItemStates[stateUUID] = { service: serviceName, state: stateName };
         this.Service[serviceName] = new MotionSensor(this.platform, this.Accessory!, matchingDevice); // Register Intercom Motion Sensor
       }
-    } else {
-      this.platform.log.debug(`[${this.device.name}] Unable to find Loxone Intercom MotionSensor`);
     }
   }
 }

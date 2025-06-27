d98fd9939bd36851bf10ccdb895bf5fe16d487bc
Rudy <rudy.berends@visolity.nl>
2025-06-27 15:36:55 +0000

    fix: refactor radioitems

--- src/loxone/items/Radio.ts
+++ src/loxone/items/Radio.ts
@@ -1,15 +1,16 @@
 import { LoxoneAccessory } from '../../LoxoneAccessory';
 import { Outlet } from '../../homekit/services/Outlet';
+import { Switch as SwitchService } from '../../homekit/services/Switch';
+import { Switch } from './Switch'; // To access determineSwitchType
 
 /**
  * Loxone Radio Item
-*/
+ */
 export class Radio extends LoxoneAccessory {
 
   configureServices(): void {
-
     this.ItemStates = {
-      [this.device.states.activeOutput]: { 'service': 'PrimaryService', 'state': 'activeOutput' },
+      [this.device.states.activeOutput]: { service: 'PrimaryService', state: 'activeOutput' },
     };
 
     this.registerRadioSwitches();
@@ -17,18 +18,26 @@ export class Radio extends LoxoneAccessory {
 
   // Create Radio Switches
   registerRadioSwitches(): void {
-
-    this.device.details!.outputs![0] = this.device.details.allOff; // allOff Switch
+    // Add the allOff virtual output at index 0
+    this.device.details!.outputs![0] = this.device.details.allOff;
 
     for (const radioSwitchKey in this.device.details.outputs) {
       const sanitizedRadioName = this.sanitizeLoxoneItemName(this.device.details.outputs[radioSwitchKey]);
+
       const radioItem = { ...this.device };
       radioItem.name = sanitizedRadioName;
       radioItem.type = 'Switch';
-      radioItem.cat = radioSwitchKey; // Store ID in CAT field
+      radioItem.cat = radioSwitchKey; // Store the radio ID in the cat field
       radioItem.details = {};
 
-      this.Service[radioItem.name] = new Outlet(this.platform, this.Accessory!, radioItem, this.handleLoxoneCommand.bind(this));
+      const serviceType = Switch.determineSwitchType(radioItem, this.platform.config);
+
+      this.platform.log.info(`[Radio: ${radioItem.name}] resolved to service type: ${serviceType}`);
+
+      this.Service[radioItem.name] =
+        serviceType === 'outlet'
+          ? new Outlet(this.platform, this.Accessory!, radioItem, this.handleLoxoneCommand.bind(this))
+          : new SwitchService(this.platform, this.Accessory!, radioItem, this.handleLoxoneCommand.bind(this));
     }
   }
 
@@ -36,11 +45,14 @@ export class Radio extends LoxoneAccessory {
     const currentValue = message.value as number;
 
     for (const serviceName in this.Service) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const service: any = this.Service[serviceName];
-      message.value = currentValue as number === parseInt(service.device.cat) ? 1 : 0;
+      message.value = currentValue === parseInt(service.device.cat) ? 1 : 0;
 
-      // Use bracket notation to safely access the method even if the serviceName contains spaces
-      const updateService = new Function('message', `return this.Service["${serviceName}"].updateService(message);`);
+      const updateService = new Function(
+        'message',
+        `return this.Service["${serviceName}"].updateService(message);`,
+      );
       updateService.call(this, message);
     }
   }
